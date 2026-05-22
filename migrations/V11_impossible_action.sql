c3b235dfeaa59c85376ee262cd2bcc814692c8e4
Pragada Prasanna Kumar <177929643+Prasanna18-code@users.noreply.github.com>
2026-05-22 11:36:13 +0530

    fixing sql formatting for demo 2

--- migrations/V11_impossible_action.sql
+++ migrations/V11_impossible_action.sql
@@ -1,5 +1,7 @@
--- This first command is perfectly valid:
-CREATE TABLE temporary_demo (id INT);
+CREATE TABLE temporary_demo (
+    id INT
+);
 
--- This second command will crash (because fake_table doesn't exist):
-ALTER TABLE fake_table ADD COLUMN email TEXT;
+ALTER TABLE fake_table 
+ADD COLUMN email TEXT;
+
